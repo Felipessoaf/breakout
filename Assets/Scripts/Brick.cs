03f0c7181286e88787f8e69ed4695e5ba3b89a7f
Felipe Pessoa <felipessoaf@gmail.com>
2024-07-28 18:38:17 -0300

    [wip] Spawning bricks; [wip] brick destroyed behaviour

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Breakout
 {
     public class Brick : MonoBehaviour
     {
+        [SerializeField] private BoxCollider2D BoxCollider2D;
         [SerializeField] private Animator Animator;
+        [SerializeField] private Image Image;
+
+        private int _pointsToReward;
+
+        private void Start() =>
+            BoxCollider2D.size = (transform as RectTransform).rect.size;
+
+        public void Setup(Color color, int points)
+        {
+            Image.color = color;
+            _pointsToReward = points;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.gameObject.CompareTag("Ball"))
+            {
+                //TODO: reward points
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
 
-using System.Linq;
-using System.Collections.Generic;
-
 namespace Breakout
 {
     public class Controller : MonoBehaviour
     {
         [SerializeField] private Brick BrickPrefab;
         [SerializeField] private RectTransform BrickContainer;
+
+        [SerializeField] private int AmountToSpawn;
+
+        private void Start() => SpawnBricks(AmountToSpawn);
+
+        public void SpawnBricks(int amount)
+        {
+            float brickWidth = BrickPrefab.GetComponent<RectTransform>().rect.width;
+
+            for (int i = 0; i < amount; i++)
+            {
+                Brick brick = Instantiate(BrickPrefab, Vector3.right * i * brickWidth, BrickPrefab.transform.rotation, BrickContainer);
+                brick.Setup(Color.blue, 10);
+            }
+        }
     }
 }
